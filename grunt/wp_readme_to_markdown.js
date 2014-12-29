0aa404300c66146b4b1066a610bd8504da785aa1
swissspidy <swissspidy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-29 09:12:14 +0000

    Updat
    git-svn-id: https://plugins.svn.wordpress.org/voice-search/tags/1.0.0@1056113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- grunt/wp_readme_to_markdown.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  dist: {
-    files: {
-      'README.md': 'readme.txt',
-      'screenshot_url': 'https://github.com/swissspidy/voice-search/blob/master/{screenshot}.png'
-    }
-  }
-};
